27d5606ab3f3b9b3759b8cdc4a32b890f70ca1fe
aqutw <aquarian.ex@gmail.com>
2014-09-07 14:56:30 +0800

    fix app.js view folder bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var port = process.env.PORT || 3000
 var app = express()
 
-app.set('views', '.views')
+app.set('views', './views')
 app.set('view engine', 'jade')
 app.listen(port)
 
