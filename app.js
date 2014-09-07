a46ca48a29ae1d0df9428096b07f737d4961ef14
aqutw <aquarian.ex@gmail.com>
2014-09-07 14:59:23 +0800

    commit a lose file for previous commit

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ app.set('view engine', 'jade')
 app.listen(port)
 
 console.log('express start on port '+port)
+
+app.get('/', function(req, res){
+	res.render('index', {title: 'index here'})
+})
+app.get('/movie/:id', function(req, res){
+	res.render('detail', {title: 'dtail here'})
+})
+app.get('/amdin/movie', function(req, res){
+	res.render('admin', {title: 'admin here'})
+})
+app.get('/admin/list', function(req, res){
+	res.render('list', {title: 'list here'})
+})
