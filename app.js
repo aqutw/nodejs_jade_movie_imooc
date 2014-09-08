4c1dc7b2560e053434d1084761590f6a9240bbdd
aqutw <aquarian.ex@gmail.com>
2014-09-08 18:26:18 +0800

    re-setup frontend folder and files

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost/imooc')
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
 app.use(bodyParser.urlencoded())
-app.use(express.static(path.join(__dirname, 'bower_components')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.locals.moment = require('moment')
 app.listen(port)
 
